8cbdc2548bbba6149a6d42d782ec22b3c43551bd
Hoseyn Amiri <aamirihoseyn@gmail.com>
2024-04-05 09:07:56 -0400

    debug arg

--- pyrtz2/app.py
+++ pyrtz2/app.py
@@ -4,12 +4,8 @@ from dash_bootstrap_components.themes import BOOTSTRAP
 from .src.components.layout import create_layout
 
 
-def run() -> None:
+def run(debug: bool = False) -> None:
     app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
     app.title = "AFM Curve Analysis"
     app.layout = create_layout(app)
-    app.run(debug=True)
-
-
-if __name__ == "__main__":
-    run()
+    app.run(debug=debug)
