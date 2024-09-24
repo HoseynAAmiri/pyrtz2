035b074094b5ba3e6451eaf9ac213fc8a6682341
Hoseyn Amiri <aamirihoseyn@gmail.com>
2024-09-24 13:18:26 -0400

    linking adjust to fit

--- pyrtz2/src/components/fitter.py
+++ pyrtz2/src/components/fitter.py
@@ -20,15 +20,14 @@ def render(app: Dash) -> html.Div:
         ctx = callback_context
         trigger_id = ctx.triggered[0]['prop_id'].split('.')[0]
         indentation = process_indentation(indentation)
-
         if trigger_id == ids.FIT_CHECKLIST and indentation == 0.0:
             return no_update, [], "Unable to proceed without indentation!"
 
-        if trigger_id == ids.ADJUST_CHECKLIST and fit:
-            fit = []
-
         if trigger_id == ids.ADJUST_CHECKLIST and not fit:
             return no_update
+
+        if trigger_id == ids.ADJUST_CHECKLIST and fit:
+            fit = []
 
         if trigger_id == ids.FIT_CHECKLIST and not adjust:
             adjust = [True]
